2bb85dab5b5ef5e03fdd3a68671518bc81740b48
Azekowka <gabjtov@gmail.com>
2024-10-27 19:19:38 +0500

    Update 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '', // Replace 'portfolio' with your repo name
 };
 
 export default nextConfig;
