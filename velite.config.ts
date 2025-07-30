c1a0fda7081ad69064433d12c616f2a2eccfda1b
Azekowka <gabjtov@gmail.com>
2025-07-30 20:03:45 +0500

    projects clean up

--- velite.config.ts
+++ velite.config.ts
@@ -107,25 +107,25 @@ export default defineConfig({
     rehypePlugins: [
       rehypeSlug,
       rehypeKatex,
-      [
-        rehypePrettyCode,
-        {
-          theme: "one-dark-pro",
-          onVisitLine(node: LineElement) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: "text", value: " " }];
-            }
-          },
-          onVisitHighlightedLine(node: LineElement) {
-            node.properties.className?.push("line--highlighted");
-          },
-          onVisitHighlightedWord(node: LineElement) {
-            node.properties.className = ["word--highlighted"];
-          },
-        },
-      ],
+      // [
+      //   rehypePrettyCode,
+      //   {
+      //     theme: "dark-plus",
+      //     onVisitLine(node: LineElement) {
+      //       // Prevent lines from collapsing in `display: grid` mode, and allow empty
+      //       // lines to be copy/pasted
+      //       if (node.children.length === 0) {
+      //         node.children = [{ type: "text", value: " " }];
+      //       }
+      //     },
+      //     onVisitHighlightedLine(node: LineElement) {
+      //       node.properties.className?.push("line--highlighted");
+      //     },
+      //     onVisitHighlightedWord(node: LineElement) {
+      //       node.properties.className = ["word--highlighted"];
+      //     },
+      //   },
+      // ],
       [
         rehypeAutolinkHeadings,
         {
